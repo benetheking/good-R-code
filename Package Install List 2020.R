4cd08cedd797c0f0147e4e062e33b472a8df0e5f
Benedikt <35131697+benetheking@users.noreply.github.com>
2022-03-08 13:02:56 +0100

    Add files via upload

--- Package Install List 2020.R
+++ Package Install List 2020.R
@@ -1,24 +1,39 @@
 ########################################
-###########
-## Install packages list
+############# Install packages list
+## SHINY
 install.packages("manipulateWidget")
 install.packages("shinydashboard")
-install.packages("tidymodelling")
 install.packages("shinyWidgets")
-install.packages("data.table")
+install.packages("shiny")
+
+## TIDY and TIDYMODELS
+install.packages("tidymodels")
 install.packages("tidyverse")
 install.packages("lubridate")
+install.packages("workflows")
 install.packages("ggthemes")
+install.packages("ggridges")
+install.packages("broom")
+install.packages("tune")
+install.packages("ranger")
+install.packages("caret")
+install.packages("caTools")
+
+## OTHER
+install.packages("data.table")
+install.packages("tableplot")
+
+## DEV STUFF
 install.packages("devtools")
+install.packages("rlang")
+
 install.packages("hexView")
 install.packages("prophet")
+install.packages("anytime")
 install.packages("plotly")
 install.packages("tcltk2")
 install.packages("lmtest")
 install.packages("Hmisc")
-install.packages("rlang")
-install.packages("shiny")
-install.packages("broom")
 install.packages("xlsx")
 install.packages("onls")
 install.packages("Rcpp")
@@ -27,34 +42,8 @@ install.packages("xts")
 install.packages("car")
 install.packages("pak")
 
-## REG in R
-datis <- full_data %>%
-  select(log_ELV_APO_GES70, av, rep(1, 157) )
-
-plot(datis)
-lines(predict(lm(av~., datis)))
-(lm(y~0+V1, dat2))
-x <- matrix(c(datis[,1], rep(1, 157) ), ncol=2 )
-x <- matrix(datis[,1], ncol=1 )
-y <- av
-
-dat2 <- cbind(x, y) %>% as.data.frame()
 
 beta_hat <- ((t(x)%*%x)^-1)%*%(t(x)%*%y)
+## SOLVE()!!!
 
-#### LOG REG
-library(tidyverse)
-x_1 <- sample(0:40, 30, T)
-y_1 <- ifelse(x_1 > 15, 1, 0)
-## add some noise
-x_1 <- x_1+rnorm(30, mean=5)
-daten <- data.frame(y_1, x_1)
-
-modl <- glm(y_1 ~ 0+ x_1, data=daten, family = "binomial")
-summary(modl)
-daten$preds <- predict(modl, type="response")
-predict(modl)
-plot(x=daten$x_1, y=daten$y_1)
-lines(x=daten$x_1, y=daten$preds)
-plot(x=daten$x_1, y=daten$preds)
 
