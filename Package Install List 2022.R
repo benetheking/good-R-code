f432d95ea231746ac1480537df01987ef0278d75
Benedikt <35131697+benetheking@users.noreply.github.com>
2022-03-21 17:07:30 +0100

    Add files via upload

---
+++ Package Install List 2022.R
@@ -0,0 +1,52 @@
+########################################
+############# Install packages list
+## SHINY
+install.packages("manipulateWidget")
+install.packages("shinydashboard")
+install.packages("shinyWidgets")
+install.packages("shiny")
+
+## TIDY and TIDYMODELS
+install.packages("tidymodels")
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("workflows")
+install.packages("ggthemes")
+install.packages("ggridges")
+install.packages("caTools")
+install.packages("ranger")
+install.packages("caret")
+install.packages("broom")
+install.packages("furrr")
+install.packages("tune")
+
+
+## OTHER
+install.packages("data.table")
+install.packages("tableplot")
+install.packages("tictoc")
+
+## DEV STUFF
+install.packages("devtools")
+install.packages("rlang")
+
+install.packages("hexView")
+install.packages("prophet")
+install.packages("anytime")
+install.packages("plotly")
+install.packages("tcltk2")
+install.packages("lmtest")
+install.packages("Hmisc")
+install.packages("xlsx")
+install.packages("onls")
+install.packages("Rcpp")
+install.packages("rio")
+install.packages("xts")
+install.packages("car")
+install.packages("pak")
+
+
+beta_hat <- ((t(x)%*%x)^-1)%*%(t(x)%*%y)
+## SOLVE()!!!
+
+
